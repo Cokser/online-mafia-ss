b26b94e4d320b56f245168240516fd237b340889
Valeriy Khupenia <thecokser@gmail.com>
2019-12-02 17:48:04 +0300

    added some changes in post

--- src/controllers/lobby/post.js
+++ src/controllers/lobby/post.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const uuid = require('uuid/v4');
 
 export const addLobby = (req, res) => {
-    let lobbyData;
+    let storedData;
     try {
         let rawdata = fs.readFileSync('./public/lobbies.json');
         const newLobby = {
@@ -10,16 +10,17 @@ export const addLobby = (req, res) => {
             private: req.body.private,
             description: req.body.description,
             streaming_mode: req.body.streaming_mode,
+            mode: req.body.mode,
             id: uuid(),
             utc_started: new Date().getTime(),
-            url: `lobby_${(~~(Math.random()*1e8)).toString(16)}`,
+            url: (~~(Math.random()*1e8)).toString(16),
         };
 
-        lobbyData = JSON.parse(rawdata);
-        lobbyData.data.push(newLobby);
-        const newData = JSON.stringify(lobbyData);
+        storedData = JSON.parse(rawdata);
+        storedData.data.push(newLobby);
+        const newData = JSON.stringify(storedData);
         fs.writeFileSync('./public/lobbies.json', newData);
-        res.end();
+        res.send(newLobby.url);
     }
     catch (e) {
         console.log(e.message);
