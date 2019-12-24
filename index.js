8fe93783ccb79c5ccc8a0d74054e1ed4bbe1a14b
Valeriy Khupenia <thecokser@gmail.com>
2019-12-24 17:13:36 +0300

    added some changes into initialization

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const cors = require('cors');
+require('dotenv').config();
+
+
 const userController = require('./src/controllers/user');
 import lobbyRouter from './src/routes/lobby';
 import usersRouter from './src/routes/users';
@@ -10,13 +15,19 @@ const app = express();
 app.use(cors({
     exposedHeaders: ['Link']
 }));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session({
+    cookie: { maxAge: 60000 },
+    secret: 'moprojectsecret',
+    saveUninitialized: false,
+    resave: false
+}));
 
 app.use('/lobby', lobbyRouter);
 app.use('/users', usersRouter);
 
 const port = 3200;
 
-app.listen(port);
+app.listen(process.env.APP_PORT);
