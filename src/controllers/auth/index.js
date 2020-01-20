4bb808d0da04d6ac2cad0bd0e831ba4a1b1ab52b
Valeriy Khupenia <thecokser@gmail.com>
2020-01-20 18:15:07 +0300

    Added rootRouter #2

---
+++ src/controllers/auth/index.js
@@ -0,0 +1,7 @@
+﻿export const checkAuth = async (req, res) => {};
+
+export const registerAccount = async (req, res) => {};
+
+export const loginAccount = async (req, res) => {};
+
+export const logoutAccount = async (req, res) => {};
---
+++ src/routes/auth.js
@@ -0,0 +1,12 @@
+﻿import {checkAuth, loginAccount, logoutAccount, registerAccount} from "../controllers/auth";
+
+const express = require('express');
+
+const authRouter = express.Router();
+
+authRouter.get('/', checkAuth);
+authRouter.get('/register', registerAccount);
+authRouter.get('/login', loginAccount);
+authRouter.get('/logout', logoutAccount);
+
+export default authRouter;
